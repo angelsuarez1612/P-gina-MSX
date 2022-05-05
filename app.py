0b4851960a8cab9762313690e0dfaf412fe4eadc
angelsuarez1612 <98739735+angelsuarez1612@users.noreply.github.com>
2022-05-05 19:18:38 +0200

    Actualizado app.py con detalles y juegos

--- app.py
+++ app.py
@@ -10,3 +10,48 @@ with open("MSX.json") as fich:
 @app.route('/',methods=["GET","POST"])
 def inicio():
 	return render_template("inicio.html")
+
+#Página Juegos
+@app.route('/juegos',methods=["GET","POST"])
+def juegos():
+	categorias=[]
+	for i in todatos:
+		categorias.append(str(i["categoria"]))
+	categorias=list(set(categorias))
+	if request.method=="GET":
+		return render_template("juegos.html",categorias=categorias)
+	else:
+		try:
+			cadena=request.form.get("name")
+		except:
+			abort(404)
+		categoria=request.form.get("cat")
+		listajuegos=[]
+		listaidentificadores=[]
+		listadesarrolladores=[]
+		for dat in todatos:
+			if cadena in str(dat["nombre"]) and categoria == str(dat["categoria"]):
+				listajuegos.append(str(dat["nombre"]))
+				listaidentificadores.append(str(dat["id"]))
+				listadesarrolladores.append(str(dat["desarrollador"]))
+				todo=zip(listajuegos,listadesarrolladores,listaidentificadores,categorias)
+			elif cadena == "" and categoria == "":
+				listajuegos.append(str(dat["nombre"]))
+			elif cadena in str(dat["nombre"]) and categoria == "Todos":
+				listajuegos.append(str(dat["nombre"]))
+				listadesarrolladores.append(str(dat["desarrollador"]))
+				listaidentificadores.append(str(dat["id"]))
+				todo=zip(listajuegos,listadesarrolladores,listaidentificadores,categorias)
+
+		return render_template("juegos.html",juegos=todo,cadena=cadena,categorias=categorias,categoria=categoria)
+
+#Página de detalles sobre un juego
+@app.route('/juego/<int:identificador>', methods=["GET","POST"])
+def detallejuego(identificador):
+	for a in todatos:
+		if a["id"] == identificador:
+			return render_template("detallejuego.html",detalle=a)
+	abort(404)
+
+port=os.environ["PORT"]
+app.run('0.0.0.0', int(port), debug=False)
